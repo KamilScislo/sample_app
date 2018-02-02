86a16414e8ce00adf63ce1d6f6530459f0b8cf68
KamilScislo <kamil.m.scislo@gmail.com>
2018-02-02 09:05:15 +0100

    Make signup working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,18 @@ class UsersController < ApplicationController
   end
 
   def create
-  @user = User.new(user_params)
-  if @user.save
-    # Handle a successful save.
-  else
-    render 'new'
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
-end
 
-private
+  private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
